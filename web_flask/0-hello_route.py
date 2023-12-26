43242c019b839989f73abe8c93cae1cbae27b788
everlyne akinyi <evahaudi2354@gmail.com>
2023-12-26 02:57:59 -0800

    uod

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,18 +1,15 @@
-#!/usr/my_env/bin/python3
-"""Start a flask web app
-"""
-
+#!/usr/bin/python3
+""" Script that runs an app with Flask framework """
 from flask import Flask
+
+
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_flask():
-    """Return string when route queried
-    """
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ Function called with / route """
     return 'Hello HBNB!'
 
-if __name__ == '__main__':
-    app.url_map.strict_slashes = False
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
-
